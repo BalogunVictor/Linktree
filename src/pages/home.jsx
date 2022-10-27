71445b53aa60c06429c7cd66ea88c0ce358e9d58
balogunvictor <praisetheultimate@gmail.com>
2022-10-27 07:09:32 +0100

    Linktree ui with Profile section and Links section

---
+++ src/pages/home.jsx
@@ -0,0 +1,21 @@
+import './home.style.css'
+
+export default function Home() {
+  return (
+    <div className="home">
+
+      <div id="profile">
+        <img src="" alt="" id="profile__Img" />
+         <div id="profileName">Victor Balogun</div>
+      </div>
+
+      <div id="links">
+          <a href="" id='twitter'>Twitter</a>
+          <a href="" id='btn__zuri'>Zuri</a>
+          <a href="" id='book__python'>Book Python</a>
+          <a href="" id='pitch'>Pitch</a>
+          <a href="" id='book__design'>Book Design</a>
+      </div>`
+     </div> 
+  )
+}
