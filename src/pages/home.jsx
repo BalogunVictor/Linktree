8aa6d57142275f474329efc806cfb16835f9db35
balogunvictor <praisetheultimate@gmail.com>
2022-10-28 07:36:56 +0100

    added profile Image and links to open in a new tab

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,19 +2,23 @@ import './home.style.css'
 
 export default function Home() {
   return (
+
+   
     <div className="home">
 
       <div id="profile">
-        <img src="" alt="" id="profile__Img" />
-         <div id="profileName">Victor Balogun</div>
+        <img src="/assets/person1.jpg" alt="profileImg" id="profile__Img" />
+        <div id="profileName">Victor Balogun</div>
+        <a href="https://training.zuri.team/" target="_blank" rel='noopener noreferrer' id='btn__zuri'>Twitter</a>
+         
       </div>
 
       <div id="links">
-          <a href="" id='twitter'>Twitter</a>
-          <a href="" id='btn__zuri'>Zuri</a>
-          <a href="" id='book__python'>Book Python</a>
-          <a href="" id='pitch'>Pitch</a>
-          <a href="" id='book__design'>Book Design</a>
+          <a href="https://training.zuri.team/" target="_blank" rel='noopener noreferrer' id='btn__zuri'>Zuri</a>
+          <a href="http://books.zuri.team " target="_blank" rel='noopener noreferrer' id='books'>Books</a>
+          <a href="https://books.zuri.team/python-for-beginners?ref_id=victorbalogun" target="_blank" rel='noopener noreferrer' id='book__python'>Book Python</a>
+          <a href="https://background.zuri.team," target="_blank" rel='noopener noreferrer' id='pitch'>Pitch</a>
+          <a href="https://books.zuri.team/design-rules"  target="_blank" rel='noopener noreferrer' id='book__design'>Book Design</a>
       </div>`
      </div> 
   )
