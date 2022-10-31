e447ed811e33031f7c218c1a374dbc48bb369a6c
Balogun Victor <93051955+BalogunVictor@users.noreply.github.com>
2022-10-31 09:25:40 +0100

    Update home.jsx

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -17,7 +17,7 @@ export default function Home() {
           <a href="https://training.zuri.team/" target="_blank" rel='noopener noreferrer' id='btn__zuri'>Zuri</a>
           <a href="http://books.zuri.team " target="_blank" rel='noopener noreferrer' id='books'>Books</a>
           <a href="https://books.zuri.team/python-for-beginners?ref_id=victorbalogun" target="_blank" rel='noopener noreferrer' id='book__python'>Book Python</a>
-          <a href="https://background.zuri.team," target="_blank" rel='noopener noreferrer' id='pitch'>Pitch</a>
+          <a href="https://background.zuri.team" target="_blank" rel='noopener noreferrer' id='pitch'>Pitch</a>
           <a href="https://books.zuri.team/design-rules"  target="_blank" rel='noopener noreferrer' id='book__design'>Book Design</a>
       </div>`
      </div> 
